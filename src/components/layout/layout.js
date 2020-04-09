8f70d2b5d8e21a3bcc80aaec0f342d7e8e31a983
Developer Jahid <jhpabel@gmail.com>
2020-04-09 10:10:04 +0600

    netlifycms

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,14 +1,12 @@
 import React, { Fragment} from "react"
 import PropTypes from "prop-types"
 import Navbar from "./navbar/navbar"
-import Footer from "./footer/footer"
 
 const Layout = ({ children }) => {
   return (
     <Fragment>
       <Navbar />
         {children}
-      <Footer />
     </Fragment>
   )
 }
---
+++ src/components/layout/navbar/nav/navme.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react"
+import { Navbar } from "reactstrap"
+
+const Navme = () => {
+    return(
+        <Fragment>
+            <Navbar className="navbar-expand-lg">
+                nav
+            </Navbar>
+        </Fragment>
+    )
+}
+
+export default Navme
